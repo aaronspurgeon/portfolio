61122d890dc3a65de5d06ea993fbf74d5ae334bf
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-07-12 19:40:30 -0400

    Making progress with the landing css

--- portfolio/src/components/Landing/Landing.jsx
+++ portfolio/src/components/Landing/Landing.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Landing.scss'
+import NavBar from '../NavBar/NavBar'
 
 export default function Landing() {
     return (
         <div>
-            this is the Landing
+            <div className="landing-text">
+                <div className="landing-flex">
+                    <p>Logo</p>
+                    <h1>Aaron<br />Spurgeon</h1>
+                    <div className="social">
+                        <a href='https://twitter.com/unalikez' target="_blank">Twitter</a>
+                        <a href='https://www.instagram.com/desolatexciv/' target="_blank">Instagram</a>
+                        <a href='https://www.linkedin.com/in/aaron-spurgeon-9919a7183/' target="_blank">LinkedIn</a>
+                    </div>
+                </div>
+                <div className="landing-flex">
+                    <NavBar />
+                </div>
+            </div>
         </div>
     )
 }
--- portfolio/src/components/NavBar/NavBar.jsx
+++ portfolio/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router'
 import './NavBar.scss'
 
 export default function NavBar() {
     return (
         <div>
-
+            <a href="#">Services</a>
+            <a href="#">Works</a>
+            <a href="#">Blog</a>
         </div>
     )
 }
