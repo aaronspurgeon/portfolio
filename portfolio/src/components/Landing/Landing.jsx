20ee5da76ab7abd42c24954b02ea2ec74187c4ce
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-07-13 16:35:35 -0400

    Added icons

--- portfolio/src/components/Landing/Landing.jsx
+++ portfolio/src/components/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Landing.scss'
 import NavBar from '../NavBar/NavBar'
+import { AiFillGithub, AiFillTwitterCircle, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
 
 export default function Landing() {
     return (
@@ -10,13 +11,17 @@ export default function Landing() {
                     <p>Logo</p>
                     <h1>Aaron<br />Spurgeon</h1>
                     <div className="social">
-                        <a href='https://twitter.com/unalikez' target="_blank">Twitter</a>
-                        <a href='https://www.instagram.com/desolatexciv/' target="_blank">Instagram</a>
-                        <a href='https://www.linkedin.com/in/aaron-spurgeon-9919a7183/' target="_blank">LinkedIn</a>
+                        <a href='https://github.com/aaronspurgeon' target="_blank"><AiFillGithub size={36} /></a>
+                        <a href='https://twitter.com/unalikez' target="_blank"><AiFillTwitterCircle size={36} /></a>
+                        <a href='https://www.instagram.com/desolatexciv/' target="_blank"><AiFillInstagram size={36} /></a>
+                        <a href='https://www.linkedin.com/in/aaron-spurgeon-9919a7183/' target="_blank"><AiFillLinkedin size={36} /></a>
                     </div>
                 </div>
-                <div className="landing-flex">
+                <div className="landing-flex second">
                     <NavBar />
+                    <p>- Introduction</p>
+                    <h3>Full Stack Developer, based in New Hampshire</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus est pariatur quibusdam a alias praesentium iste temporibus, officiis officia commodi magnam quas nostrum possimus aut consectetur nulla velit laboriosam voluptatum?</p>
                 </div>
             </div>
         </div>
