3b1c72652024fb031cc595427764bfb1a75184a8
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-07-13 17:52:34 -0400

    Stopping point

--- portfolio/src/components/Landing/Landing.jsx
+++ portfolio/src/components/Landing/Landing.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import './Landing.scss'
 import NavBar from '../NavBar/NavBar'
 import { AiFillGithub, AiFillTwitterCircle, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
+import img from '../../images/portfolio.png'
 
 export default function Landing() {
     return (
         <div>
             <div className="landing-text">
                 <div className="landing-flex">
-                    <p>Logo</p>
-                    <h1>Aaron<br />Spurgeon</h1>
+                    <h1 className='name'>Aaron<br />Spurgeon.</h1>
                     <div className="social">
-                        <a href='https://github.com/aaronspurgeon' target="_blank"><AiFillGithub size={36} /></a>
-                        <a href='https://twitter.com/unalikez' target="_blank"><AiFillTwitterCircle size={36} /></a>
-                        <a href='https://www.instagram.com/desolatexciv/' target="_blank"><AiFillInstagram size={36} /></a>
-                        <a href='https://www.linkedin.com/in/aaron-spurgeon-9919a7183/' target="_blank"><AiFillLinkedin size={36} /></a>
+                        <a href='https://github.com/aaronspurgeon' target="_blank" className='icons'><AiFillGithub size={36} /></a>
+                        <a href='https://twitter.com/unalikez' target="_blank" className='icons'><AiFillTwitterCircle size={36} /></a>
+                        <a href='https://www.instagram.com/desolatexciv/' target="_blank" className='icons'><AiFillInstagram size={36} /></a>
+                        <a href='https://www.linkedin.com/in/aaron-spurgeon-9919a7183/' target="_blank" className='icons'><AiFillLinkedin size={36} /></a>
                     </div>
                 </div>
                 <div className="landing-flex second">
                     <NavBar />
-                    <p>- Introduction</p>
-                    <h3>Full Stack Developer, based in New Hampshire</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus est pariatur quibusdam a alias praesentium iste temporibus, officiis officia commodi magnam quas nostrum possimus aut consectetur nulla velit laboriosam voluptatum?</p>
+                    <p className='intro-text'>- Introduction</p>
+                    <h3 className='title'>Full Stack Developer, based in New Hampshire</h3>
+                    <p className='summary'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus est pariatur quibusdam a alias praesentium iste temporibus.</p>
                 </div>
             </div>
         </div>
